e472947ef1f59981b0f2141343db14e2cf576313
k0lm1r <andrej88k0t0v@gmail.com>
2025-08-14 01:29:05 +0300

    draft for service

--- src/main/java/transactions/service/ProcessingService.java
+++ src/main/java/transactions/service/ProcessingService.java
@@ -1,9 +1,31 @@
 package transactions.service;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
 import transactions.db.*;
+import transactions.models.Transaction;
 
 public class ProcessingService {
+    private BlockingQueue<Transaction> transactionsQueue = new LinkedBlockingQueue<>();
+
     public ProcessingService () {
+        new TransacitonDB().createTable();
+
+        if (new AccountDB().createTable()) {
+
+        }
+    }
+
+    public synchronized boolean addToQueue(Transaction newTransaction) {
+        return transactionsQueue.add(newTransaction);
+    }
+
+    public void processQueue() {
+        ExecutorService pool = Executors.newWorkStealingPool(4);
+        
         
     }
 }
